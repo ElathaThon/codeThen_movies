b21d05f7f36730a9fe2cb058e5b40f4cc89b9ed6
Elatha <bernat.90@gmail.com>
2017-09-16 16:25:54 +0200

    Implemented methods getById, findByMovie, findByUser, AddRental and updateRental in the class RentalService

--- src/main/java/com/topoinventari/movies/models/Rental.java
+++ src/main/java/com/topoinventari/movies/models/Rental.java
@@ -1,5 +1,7 @@
 package com.topoinventari.movies.models;
 
+import com.topoinventari.movies.services.MovieService;
+
 /**
  * The rentals of the movies with the client that have it
  */
@@ -9,6 +11,10 @@ public class Rental {
 	private int idMovie;
 	private int idUser;
 	private int days;
+
+	private MovieService movieService = new MovieService();
+	//TODO: private UserService userService = new UserService();
+
 
 	public Rental(int id, int idMovie, int idUser, int days) {
 		this.id = id;
@@ -31,5 +37,30 @@ public class Rental {
 
 	public int getDays() {
 		return days;
+	}
+
+	public String getMovieTitle() {
+		return movieService.getById(idMovie).getTitle();
+	}
+
+	public String getUserName() {
+		//TODO: return UserService.getById(idUser).getName();
+		return "abc";
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setIdMovie(int idMovie) {
+		this.idMovie = idMovie;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public void setDays(int days) {
+		this.days = days;
 	}
 }
--- src/main/java/com/topoinventari/movies/services/RentalService.java
+++ src/main/java/com/topoinventari/movies/services/RentalService.java
@@ -2,8 +2,10 @@ package com.topoinventari.movies.services;
 
 import com.topoinventari.movies.models.Rental;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Service to manage {@link Rental}
@@ -23,6 +25,67 @@ public class RentalService {
 		rentals.put(3, new Rental(3,3,2,6));
 
 		nextId = 4;
+
+	}
+
+	public Rental getById(int id) {
+		return rentals.get(id);
+	}
+
+	public Collection<Rental> findByMovie(String search) {
+
+		final Collection<Rental> result;
+
+		if (search != null) {
+			result = this.rentals.values().stream()
+					.filter(rental -> rental.getMovieTitle().toLowerCase().contains(search.toLowerCase()))
+					.collect(Collectors.toList());
+
+		} else {
+			result = this.rentals.values();
+		}
+
+		return result;
+
+	}
+
+	public Collection<Rental> findByUser(String search) {
+
+		final Collection<Rental> result;
+
+		if (search != null) {
+			result = this.rentals.values().stream()
+					.filter(rental -> rental.getUserName().toLowerCase().contains(search.toLowerCase()))
+					.collect(Collectors.toList());
+
+		} else {
+			result = this.rentals.values();
+		}
+
+		return result;
+
+	}
+
+	public Rental addRental(Rental rental) {
+
+		rental.setId(nextId);
+		rentals.put(nextId, rental);
+
+		nextId++;
+		System.out.println(rental);
+		return rental;
+
+	}
+
+	public Rental updateRental(int id, Rental rental) {
+
+		Rental editRental = getById(id);
+
+		editRental.setIdUser(rental.getIdUser());
+		editRental.setIdMovie(rental.getIdMovie());
+		editRental.setDays(rental.getDays());
+
+		return editRental;
 
 	}
 
